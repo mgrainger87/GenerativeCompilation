7cf303de9246f134f4b0354f9d5d02d72a6d1ed6
Morgan Grainger <morgan.grainger@gmail.com>
2023-09-03 13:07:37 -0500

    Move analysis directory

--- visualize_results.py
+++ visualize_results.py
@@ -3,17 +3,16 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import sys
-import seaborn as sns
 
 # Check if the user has provided a command-line argument
-if len(sys.argv) < 2:
-    print("Please provide the folder path as a command-line argument.")
+if len(sys.argv) < 3:
+    print("Please provide the problem and analysis paths as command-line arguments.")
     sys.exit(1)
 
 
 # Load data from directory
 problems_dir = sys.argv[1]
-analysis_dir = os.path.join(problems_dir, 'analysis')
+analysis_dir = sys.argv[2]
 
 if not os.path.exists(analysis_dir):
     os.makedirs(analysis_dir)
@@ -51,27 +50,27 @@ combined_df.to_csv(os.path.join(analysis_dir, 'combined_data.csv'), index=False)
 filename_renaming = {
     "clang_generated_O3_optimized.asm": "Clang O3",
     "clang_generated_llm_optimized.asm": "LLM optimized",
-    "clang_generated_unoptimized.asm": "Unoptimized"
+    "clang_generated_unoptimized.asm": "Clang Unoptimized"
 }
-combined_df['Approach'] = combined_df['Filename'].replace(filename_renaming)
+combined_df['Generation Method'] = combined_df['Filename'].replace(filename_renaming)
 
-# Dynamically assign colors based on unique approaches
-unique_approaches = combined_df['Approach'].unique()
-colors = dict(zip(unique_approaches, sns.color_palette("husl", len(unique_approaches))))
+# Set colors based on the presence of "LLM" in the Generation Method
+unique_generation_methods = combined_df['Generation Method'].unique()
+colors = {method: 'blue' if "LLM" in method else 'lightgray' for method in unique_generation_methods}
 
 # Generate horizontal bar charts for each problem
 problems_list = combined_df['problem'].unique()
 for problem in problems_list:
     subset = combined_df[combined_df['problem'] == problem]
     
-    # Check if there are any missing approach names and skip them
-    valid_approaches = subset['Approach'].dropna().unique()
-    valid_colors = [colors[val] for val in valid_approaches if val in colors]
+    # Check if there are any missing generation method names and skip them
+    valid_generation_methods = subset['Generation Method'].dropna().unique()
+    valid_colors = [colors[val] for val in valid_generation_methods if val in colors]
     
     # Plotting
     plt.figure(figsize=(10, 3))
-    subset[subset['Approach'].isin(valid_approaches)].plot(
-        x='Approach', y='Normalized CPU Time', kind='barh', color=valid_colors, legend=False
+    subset[subset['Generation Method'].isin(valid_generation_methods)].plot(
+        x='Generation Method', y='Normalized CPU Time', kind='barh', color=valid_colors, legend=False
     )
     plt.title(f"Problem {problem}")
     plt.xlabel("Normalized CPU Time")
@@ -79,26 +78,24 @@ for problem in problems_list:
     plt.tight_layout()
     plt.savefig(os.path.join(analysis_dir, f'problem_{problem}_chart.png'))
     plt.close()
- 
+    
+    
+
 # Now, generate charts based on technique
 techniques_list = combined_df['technique'].unique()
- 
+
 for technique in techniques_list:
     subset = combined_df[combined_df['technique'] == technique]
     
-    # Group by approach and calculate the mean for 'Normalized CPU Time'
-    grouped_means = subset.groupby('Approach')['Normalized CPU Time'].mean().reset_index().sort_values(by='Normalized CPU Time', ascending=True)
-    
-    # Dynamically assign colors based on unique approaches
-    unique_approaches = grouped_means['Approach'].unique()
-    colors = dict(zip(unique_approaches, sns.color_palette("husl", len(unique_approaches))))
+    # Group by generation method and calculate the mean for 'Normalized CPU Time'
+    grouped_means = subset.groupby('Generation Method')['Normalized CPU Time'].mean().reset_index().sort_values(by='Normalized CPU Time', ascending=True)
     
     # Plotting
     plt.figure(figsize=(10, 5))
-    plt.barh(grouped_means['Approach'], grouped_means['Normalized CPU Time'], color=[colors[val] for val in unique_approaches])
+    plt.barh(grouped_means['Generation Method'], grouped_means['Normalized CPU Time'], color=[colors[val] for val in unique_generation_methods])
     plt.xlabel('Average Normalized CPU Time')
-    plt.ylabel('Approach')
-    plt.title(f'Average Performance by Approach for {technique}')
+    plt.ylabel('Generation Method')
+    plt.title(f'Average Performance by Generation Method for {technique}')
     plt.grid(axis='x', linestyle='--', linewidth=0.5, alpha=0.7)
     plt.tight_layout()
     plt.savefig(os.path.join(analysis_dir, f'average_performance_for_{technique}.png'))
