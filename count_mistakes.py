98af61ee4300cf30c91ab29f1892fcfdcec4aa77
Morgan Grainger <morgan.grainger@gmail.com>
2023-09-12 20:54:31 -0500

    Rename

--- count_mistakes.py
+++ count_mistakes.py
@@ -1,4 +1,3 @@
-
 import os
 import csv
 from collections import defaultdict
@@ -6,11 +5,12 @@ import sys
 from run_context import ModelContext
 
 def count_failures(directory_path):
-    # Define failure types
+    # Define failure types and prefixes
     failure_types = ["COMPILER", "LINKER", "EXECUTION", "CORRECTNESS"]
+    failure_prefixes = ["generation_failure", "optimization_failure"]
     
     # Initialize a dictionary to count the failures
-    failures_count = defaultdict(int)
+    failures_count = defaultdict(lambda: defaultdict(int))
 
     # Traverse the directory and count failures
     for root, dirs, files in os.walk(directory_path):
@@ -18,8 +18,9 @@ def count_failures(directory_path):
         if "__MACOSX" not in root:
             for file in files:
                 for failure_type in failure_types:
-                    if failure_type in file:
-                        failures_count[failure_type] += 1
+                    for prefix in failure_prefixes:
+                        if failure_type in file and file.startswith(prefix):
+                            failures_count[prefix][failure_type] += 1
 
     return failures_count
 
@@ -27,9 +28,10 @@ def write_to_csv(failures_count, output_path):
     # Write the counts to the CSV file
     with open(output_path, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Failure Type', 'Count'])
-        for failure_type, count in failures_count.items():
-            csvwriter.writerow([failure_type, count])
+        csvwriter.writerow(['Failure Type', 'Prefix', 'Count'])
+        for prefix, failure_dict in failures_count.items():
+            for failure_type, count in failure_dict.items():
+                csvwriter.writerow([failure_type, prefix, count])
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -47,5 +49,3 @@ if __name__ == "__main__":
             csv_path = os.path.join(problem.failurePath(), "failure_types.csv")
             write_to_csv(failures_count, csv_path)
             print(f"Results written to: {csv_path}")
-            
-    
